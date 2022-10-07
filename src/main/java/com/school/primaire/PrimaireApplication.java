aa14c8803d167915943e9d86b9ad7f9dbb5c2fff
Souleymane Diallo <diallosouley337@gmail.com>
2022-10-07 17:40:23 +0000

    initial

---
+++ src/main/java/com/school/primaire/PrimaireApplication.java
@@ -0,0 +1,13 @@
+package com.school.primaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school/primaire/PrimaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.primaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
